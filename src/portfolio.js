b005705017c968261d6c6d017301cd234e0d0a90
Supremekeylife <103041902+Spykeylife47@users.noreply.github.com>
2022-12-26 18:37:49 +0100

    Initialize update-certifications-section

---
+++ src/portfolio.js
@@ -0,0 +1,384 @@
+﻿
+/* Change this file to get your personal Portfolio */
+
+// Your Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  username: "Bharat Kammakatla",
+  title: "Hi all, I'm Bharat",
+  subTitle: emoji("Test Automation Engineer 👨‍💻 | Data Science Enthusiast 📊 | Tech Writer ✍️ "),
+  resumeLink: "https://drive.google.com/file/d/1DbIWFiA1l_5aVrBcbkMsimHM5h5xCKtn/view?usp=sharing"
+};
+
+// Your Social Media Link
+
+const socialMediaLinks = {
+
+  github: "https://github.com/bharatkammakatla",
+  linkedin: "https://www.linkedin.com/in/bharatkammakatla",
+  gmail: "bharatkammakatla@gmail.com"
+};
+
+// Your Skills Section
+
+const skillsSection = {
+  title: "What i do",
+  subTitle: "HANDLING ALL TEST AUTOMATION ACTIVITIES AND EXPLORING DATA SCIENCE",
+  skills: [
+    emoji("⚡ Designing and developing Test Automation frameworks using Python and Java"),
+    emoji("⚡ End to End automation executions"),
+    emoji("⚡ Working on test automation tools like Selenium, Appium, UFT, Seetest mobile cloud administration, etc"),
+    emoji("⚡ Building predictive models to extract insights and make decisions out of data")
+  ],
+
+
+  softwareSkills: [
+    {
+      skillName: "python",
+      fontAwesomeClassname: "logos:python",
+      style: {
+        backgroundColor: "transparent"
+      }
+    },
+    {
+      skillName: "java",
+      fontAwesomeClassname: "logos:java",
+      style: {
+        backgroundColor: "transparent"
+      }
+    },
+    {
+      skillName: "selenium",
+      fontAwesomeClassname: "logos:selenium",
+      style: {
+        backgroundColor: "transparent"
+      }
+    },
+    {
+      skillName: "cucumber",
+      fontAwesomeClassname: "logos:cucumber",
+      style: {
+        backgroundColor: "transparent"
+      }
+    },
+    {
+      skillName: "appium",
+      fontAwesomeClassname: "logos:appium",
+      style: {
+        backgroundColor: "transparent"
+      }
+    },
+    {
+      skillName: "maven",
+      fontAwesomeClassname: "simple-icons:apachemaven",
+      style: {
+        backgroundColor: "transparent",
+        color: "#BF0353"
+      }
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+      style: {
+        backgroundColor: "transparent"
+      }
+    },
+    {
+      skillName: "jenkins",
+      fontAwesomeClassname: "simple-icons:jenkins",
+      style: {
+        backgroundColor: "transparent",
+        color: "#34495E"
+      }
+    },
+    {
+      skillName: "postgres",
+      fontAwesomeClassname: "logos:postgresql",
+      style: {
+        backgroundColor: "transparent"
+      }
+    },
+    {
+      skillName: "tensorflow",
+      fontAwesomeClassname: "logos-tensorflow",
+      style: {
+        backgroundColor: "transparent"
+      }
+    },
+    {
+      skillName: "keras",
+      fontAwesomeClassname: "simple-icons:keras",
+      style: {
+        backgroundColor: "white",
+        color: "#D00000"
+      }
+    },
+    {
+      skillName: "flask",
+      fontAwesomeClassname: "simple-icons:flask",
+      style: {
+        backgroundColor: "transparent"
+      }
+    },
+    {
+      skillName: "reactJS",
+      fontAwesomeClassname: "simple-icons:react",
+      style: {
+        color: "#61DAFB"
+      }
+    }
+
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: false, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Test Automation",  //Insert stack or technology you have experience in
+      progressPercentage: "90%"  //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Data Science",  //Insert stack or technology you have experience in
+      progressPercentage: "70%"  //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "80%"
+    }
+  ]
+};
+
+
+
+// Your top 3 work experiences
+
+const workExperiences = {
+  viewExperiences: false, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Software Engineer",  
+      company: "Facebook",
+      companylogo: require("./assets/images/facebookLogo.png"),
+      date: "June 2018 – Present",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      descBullets: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      ]
+    },
+    {
+      role: "Front-End Developer",   
+      company: "Quora",
+      companylogo: require("./assets/images/quoraLogo.png"),
+      date: "May 2017 – May 2018",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      role: "Software Engineer Intern",  
+      company: "Airbnb",
+      companylogo: require("./assets/images/airbnbLogo.png"),
+      date: "Jan 2015 – Sep 2015",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "BharatKammakatla", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile :"true" // Set true or false to show Contact profile using Github, defaults to false 
+};
+
+
+// Some Big Projects You have worked with your company
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      link: "http://saayahealth.com/"
+    },
+    {
+      image: require("./assets/images/nextuLogo.webp"),
+      link: "http://nextu.se/"
+    }
+  ]
+};
+
+// Your Achievement
+
+const achievementSection = {
+
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle: "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achivementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle: "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      footerLink: [
+        { name: "Certification", url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing" },
+        { name: "Award Letter", url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing" },
+        { name: "Google Code-in Blog", url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html" }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      footerLink: [{ name: "View Google Assistant Action", url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en" }]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      footerLink: [
+        { name: "Certification", url: "" },
+        { name: "Final Project", url: "https://pakistan-olx-1.firebaseapp.com/" }
+      ]
+    }
+  ]
+};
+
+// Your certifications
+
+const certifications = {
+	certifications: [
+		{
+			title: "ML and DS",
+			subtitle: "- Andrei Neagoie",
+			logo_path: "zero-to-mastery.png",
+			certificate_link:
+				"https://www.udemy.com/certificate/UC-fd118b7d-7436-442c-a7f8-5077c432d596/",
+			alt_name: "Zero To Mastery",
+			color_code: "#FFFDDD"
+		},
+		{
+			title: "Cloud Admin & Automation",
+			subtitle: "- Experitest",
+			logo_path: "seetest-logo.png",
+			certificate_link:
+				"https://drive.google.com/file/d/1qhh1ZUBB2VPrOaT6bNgiJ5Y2sHmCjvMg/view?usp=drivesdk",
+			alt_name: "Experitest",
+			color_code: "#F0FFDD"
+		},
+		{
+			title: "Selenium",
+			subtitle: "- Vskills",
+			logo_path: "vskills-logo.png",
+			certificate_link:
+				"https://www.vskills.in/certification/40215-certified-selenium-professional-bharat-babu-kammakatla",
+			alt_name: "Vskills",
+			color_code: "#FFF4F4"
+		},
+		{
+			title: "Python",
+			subtitle: "- Jose Portilla",
+			logo_path: "pierian-data-logo.png",
+			certificate_link:
+				"https://www.udemy.com/certificate/UC-a80d6e7c-e753-421d-bcee-5a7bc1769f28/",
+			alt_name: "Pierian-Data",
+			color_code: "#FEF6E6"
+		}
+	]
+};
+
+// Blogs Section
+
+const blogSection = {
+
+  title: "Blogs",
+  subtitle: "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url: "https://blog.bharatkammakatla.com/face-expression-recognition-using-keras-and-cnns-ckbevmufo0195dis1dfs10kt1",
+      title: "Building a Machine Learning model to detect human emotions",
+      description: "Are you interested to know how machines detect human emotions? If so, check out here !"
+    },
+    {
+      url: "https://blog.bharatkammakatla.com/steps-to-build-a-selenium-java-framework-ckdsi73xg005ga9s1defhemfz",
+      title: "Steps to build a Selenium Java Framework",
+      description: "Interested in building a test automation framework? If so, check out here !"
+    },
+    {
+      url: "https://medium.com/@bharatkammakatla/basic-end-to-end-scikit-learn-workflow-cd9e9a50491b?source=friends_link&sk=b828ac262c172abd7508efcbd00e5bd3",
+      title: "Basic End-to-End Scikit-Learn workflow",
+      description: "Do you want to know how a basic end-to-end scikit-learn workflow is implemented ? If so, check out here !"
+    }
+  ]
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji("I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"),
+
+  talks: [
+    {
+      title: "your title",
+      subtitle: "your subtitle",
+      slides_url: "Your slides url here",
+      event_url: "your event url",
+      image: "image url"
+    }
+  ]
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: ["Paste your podcast url here"]
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle: "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+852 51606863",
+  email_address: "bharatkammakatla@gmail.com"
+};
+
+//Twitter Section
+
+const twitterDetails = {
+
+  userName : "Bharat__K"//Replace "twitter" with your twitter username without @
+};
+
+
+  
+
+export { 
+  greeting, 
+  socialMediaLinks, 
+  skillsSection, 
+  techStack, 
+  workExperiences, 
+  openSource, 
+  bigProjects, 
+  achievementSection, 
+  blogSection, 
+  talkSection, 
+  podcastSection, 
+  contactInfo , 
+  twitterDetails,
+  certifications
+};
